e6a5858a511f30cbd359a5abd33f076f31e50e58
HoldMyTech <jennifer.oneill369@gmail.com>
2025-10-28 12:21:21 -0400

    Shooting
    My character has the ability to shoot using the mouse button (yay!)

---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 25f;
+
+    void Update()
+    {
+        // Note: "Input" must be capitalized
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+       
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f; 
+
+        
+        Vector3 shootDirection = (mousePosition - transform.position).normalized;
+
+        
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+      
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity = shootDirection * bulletSpeed;
+        }
+    }
+}
